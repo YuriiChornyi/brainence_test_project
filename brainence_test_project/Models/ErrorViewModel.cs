e6f512208d3c1239a2a7a5c805b6cc63663834e4
Yurii Chornyi <yura43528@gmail.com>
2018-05-08 17:07:35 +0300

    project init

---
+++ brainence_test_project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace brainence_test_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
