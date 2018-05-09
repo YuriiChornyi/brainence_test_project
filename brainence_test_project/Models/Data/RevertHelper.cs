24c554305ae0bff4fa62f91706038a446d9ed8ac
Yurii Chornyi <yura43528@gmail.com>
2018-05-09 08:34:18 +0300

    revert helper

---
+++ brainence_test_project/Models/Data/RevertHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace brainence_test_project.Models.Data
+{
+    public static class RevertHelper
+    {
+        public static string Revert(string reverted)
+        {
+           return new string(reverted.Select(x => x).Reverse().ToArray());
+        }
+    }
+}
