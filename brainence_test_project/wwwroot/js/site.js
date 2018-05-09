ae10929d4f2ea532161be8eb0027c21d1e08d73f
Yurii Chornyi <yura43528@gmail.com>
2018-05-09 10:16:48 +0300

    few minor fixes

--- brainence_test_project/wwwroot/js/site.js
+++ brainence_test_project/wwwroot/js/site.js
@@ -3,10 +3,7 @@
         $("#fileInput").change(
             function(){
                 if ($(this).val()) {
-                    alert("file selected");
                     $('input:submit').attr('disabled',false);
-                    // or, as has been pointed out elsewhere:
-                    // $('input:submit').removeAttr('disabled'); 
                 }
             }
         );
